c35eaf334ad7bbeb9fbbb84298b02f8ed0717fa0
luispenaranda08 <146387877+luispenaranda08@users.noreply.github.com>
2025-05-19 18:47:25 -0500

    Configura script prestart y Procfile

---
+++ prestart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Simula tu ruta local en el servidor Linux
+mkdir -p /tmp/users/Karina\ Perez/Downloads/
+# Copia el CSV desde tu repositorio a la ruta simulada
+cp ./app/data/DatasetWS\(csv\).csv /tmp/users/Karina\ Perez/Downloads/
+echo "¡Estructura de carpetas creada y archivo copiado!"
